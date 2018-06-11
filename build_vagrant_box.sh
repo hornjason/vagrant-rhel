4b390f46148ccec77344b9d58b383baaa890c9a1
Jason Horn <jhorn@redhat.com>
2018-06-11 16:35:00 -0400

    added packer config for rhel7.5

--- build_vagrant_box.sh
+++ build_vagrant_box.sh
@@ -3,7 +3,9 @@
 # Used as reference.
 ISO="$1"
 #NAME="$2"
-DISTRO=$(uname -a)
+DISTRO=$(uname -a|awk '{print tolower($1)}')
+#PROVIDER=virtualbox-iso
+PROVIDER=qemu
 # assumes iso is already downloaded
 #ISO_LINK=
 read "RHN USERNAME"
@@ -28,14 +30,15 @@ fi
 #check_packer 
 if [[ -z $(packer --version) ]]; then
   echo "Installing packer"
-  wget https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_${DISTRO}_amd64.zip && \
-  unzip packer_1.1.2_darwin_amd64.zip packer -d /usr/local/bin/ && rm packer*.zip
+  wget https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_${DISTRO}_amd64.zip && \
+  unzip packer_1.2.4_${DISTROY}_amd64.zip packer -d /usr/local/bin/ && rm packer*.zip
 fi
 
 echo "ISO ${ISO}"
 git clone https://github.com/opscode/bento.git &&  cd bento
 mkdir -p iso/rhel && cd iso && ln -s $1 rhel/ && \
-packer build -only=virtualbox-iso -var mirror=file:///$(pwd) ../rhel/rhel-7.5-x86_64.json && vagrant box add
+packer build -only=virtualbox-iso -var iso_url=../iso/rhel/rhel-server-7.4-x86_64-dvd.iso rhel-7.4-x86_64.json
+packer build -only=${PROVIDER} -var iso_url=../iso/rhel/rhel-server-7.4-x86_64-dvd.iso ../rhel/rhel-7.5-x86_64.json && vagrant box add
 
 echo "Setup .env file with RHN Subscription Info"
 echo "export SUB_USER="${SUBUSER}""
